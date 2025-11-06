456ed34b55a733cc99303806a3a049c1b44e593d
gilcaspi <gilcaspi@gmail.com>
2025-11-07 00:08:23 +0200

    Raw data exploration

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -14,4 +14,25 @@ def get_claims_df() -> pd.DataFrame:
 def get_churn_labels_df() -> pd.DataFrame:
     churn_labels_file_path = os.path.join(RAW_DATA_DIRECTORY_PATH, 'churn_labels.csv')
     churn_labels_df = pd.read_csv(churn_labels_file_path)
+
+    churn_labels_df['signup_date'] = pd.to_datetime(churn_labels_df['signup_date'], format="%Y-%m-%d")
+    churn_labels_df = churn_labels_df.sort_values(by='signup_date')
     return churn_labels_df
+
+
+def get_app_usage_df() -> pd.DataFrame:
+    app_usage_file_path = os.path.join(RAW_DATA_DIRECTORY_PATH, 'app_usage.csv')
+    app_usage_df = pd.read_csv(app_usage_file_path)
+
+    app_usage_df['timestamp'] = pd.to_datetime(app_usage_df['timestamp'], format="%Y-%m-%d %H:%M:%S")
+    app_usage_df = app_usage_df.sort_values(by='timestamp')
+    return app_usage_df
+
+
+def get_web_visits_df() -> pd.DataFrame:
+    web_visits_file_path = os.path.join(RAW_DATA_DIRECTORY_PATH, 'web_visits.csv')
+    web_visits_df = pd.read_csv(web_visits_file_path)
+
+    web_visits_df['timestamp'] = pd.to_datetime(web_visits_df['timestamp'], format="%Y-%m-%d %H:%M:%S")
+    web_visits_df = web_visits_df.sort_values(by='timestamp')
+    return web_visits_df
--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -33,33 +33,80 @@ def pie_plot(
 
 def scatter_plot(
         df: pd.DataFrame,
-        x_column_name: str,
-        y_column_names: List[str],
-        title: str,
+        x_column_name: Optional[str] = None,
+        y_column_names: Optional[List[str]] = None,
+        title: Optional[str] = None,
         mode: str = "markers+lines"
 ) -> go.Figure:
     fig = go.Figure()
 
+    if x_column_name is None:
+        x_values = df.index
+        x_label = "index"
+    else:
+        x_values = df[x_column_name]
+        x_label = x_column_name
+
+    if y_column_names is None:
+        y_column_names = [col for col in df.columns if col != x_column_name]
+
+    if title is None:
+        title = f"Scatter Plot of {', '.join(y_column_names)} vs {x_label}"
+
     for col in y_column_names:
         fig.add_trace(
             go.Scatter(
-                x=df[x_column_name],
+                x=x_values,
                 y=df[col],
                 mode=mode,
                 name=col,
                 marker=dict(size=6),
                 line=dict(width=2),
-                hovertemplate=f"<b>{col}</b><br>{x_column_name}: %{{x}}<br>{col}: %{{y}}<extra></extra>"
+                hovertemplate=f"<b>{col}</b><br>{x_label}: %{{x}}<br>{col}: %{{y}}<extra></extra>"
             )
         )
 
     fig.update_layout(
         title=title,
         title_x=0.5,
-        xaxis_title=x_column_name,
+        xaxis_title=x_label,
         yaxis_title="Values",
         template="plotly_white",
         legend=dict(title="Y Columns", orientation="h", y=-0.2, x=0.5, xanchor="center"),
+        margin=dict(l=40, r=40, t=60, b=40)
+    )
+
+    return fig
+
+
+def scatter_plot_series(
+        series: pd.Series,
+        title: Optional[str] = None,
+) -> go.Figure:
+    fig = go.Figure()
+
+    if title is None:
+        title = f"Scatter Plot of {series.name} vs Index"
+
+    fig.add_trace(
+        go.Scatter(
+            x=series.index,
+            y=series.values,
+            mode="markers+lines",
+            name=series.name,
+            marker=dict(size=6),
+            line=dict(width=2),
+            hovertemplate=f"<b>{series.name}</b><br>Index: %{{x}}<br>Value: %{{y}}<extra></extra>"
+        )
+    )
+
+    fig.update_layout(
+        title=title,
+        title_x=0.5,
+        xaxis_title="Index",
+        yaxis_title="Values",
+        template="plotly_white",
+        legend=dict(title="Series", orientation="h", y=-0.2, x=0.5, xanchor="center"),
         margin=dict(l=40, r=40, t=60, b=40)
     )
 
